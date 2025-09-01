95517c8d2ba3edb9ca74800f21590e3acdddb2df
Ashot Manukyan <50967132+ashman98@users.noreply.github.com>
2025-09-02 00:06:21 +0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect('mongodb+srv://ash888848:Meri.1998@cluster0.60izxol.mongodb.net/?retryWrites=true&w=majority');
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
@@ -12,3 +12,4 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB;
+
